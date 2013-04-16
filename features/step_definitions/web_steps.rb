d1d8e406f21688f5648028fe7edcfc5db246e885
Prakash Murthy <prakblr@yahoo.com>
2013-04-16 17:34:46 +0530

    Added feature to verify editing entries functionality

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -6,10 +6,14 @@ Given(/^I fill in "(.*?)" with "(.*?)"$/) do |element, text|
   fill_in element, with: text
 end
 
-Given(/^I press "(.*?)"$/) do |element|
+Given(/^I (?:press|follow) "(.*?)"$/) do |element|
   click_on element
 end
 
 Then(/^I should see "(.*?)"$/) do |text|
   page.should have_content text
+end
+
+Then(/^I should not see "(.*?)"$/) do |text|
+  page.should_not have_content text
 end

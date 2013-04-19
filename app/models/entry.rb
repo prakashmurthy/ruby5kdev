40a66ea4940f68e23fb7270b2a119b8b53ac2254
Prakash Murthy <prakblr@yahoo.com>
2013-04-19 06:33:23 +0530

    entry => belongs_to :user

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,7 @@
 class Entry < ActiveRecord::Base
+  belongs_to :user
+
   validates :distance, :presence => { :unless => :time, :message => "You must enter distance or time or both." }
   validates :time, :presence => { :unless => :distance, :message => "You must enter distance or time or both." }
-  attr_accessible :description, :distance, :location, :time
+  attr_accessible :description, :distance, :location, :time, :user
 end

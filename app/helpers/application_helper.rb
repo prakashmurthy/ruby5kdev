5d1199e56a9a0788030bf29d88905e21a9970e0d
Prakash Murthy <prakblr@yahoo.com>
2013-04-19 23:31:48 +0530

    Added human-readable 'time since creation of entry' in entries index view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def humanize(seconds)
+    [[60, :seconds], [60, :minutes], [24, :hours], [1000, :days]].map{ |count, name|
+      if seconds > 0
+        seconds, n = seconds.divmod(count)
+        "#{n.to_i} #{name}"
+      end
+    }.compact.reverse[0..1].join( ' ' ) + " ago."
+  end
 end

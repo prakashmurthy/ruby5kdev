314362b710b6d3f62d6c9830c43f89dd27f5387f
Prakash Murthy <prakblr@yahoo.com>
2013-04-21 10:51:33 +0530

    Silly oversight in scenario set up. Fixed.

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,6 +1,6 @@
 module EntriesHelper
   def current_user_is_creator_of_entry?(entry)
-    entry.user == current_user
+    entry.user == current_user && !current_user.nil?
   end
 
   def gravatar_for(user)

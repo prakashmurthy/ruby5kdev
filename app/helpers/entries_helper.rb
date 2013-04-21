d3790439a695561016e29b1c6151e06aa481186d
Prakash Murthy <prakblr@yahoo.com>
2013-04-21 07:39:35 +0530

    Added gravatar in entries index page

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -2,4 +2,10 @@ module EntriesHelper
   def current_user_is_creator_of_entry?(entry)
     entry.user == current_user
   end
+
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.email, class: "gravatar")
+  end
 end

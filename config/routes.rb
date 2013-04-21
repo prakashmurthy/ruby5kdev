7f0a7f3e5f850a1a7438dd03b371e4f0a08cc7bf
Prakash Murthy <prakblr@yahoo.com>
2013-04-21 12:50:10 +0530

    Copied files over from old repo; files related to misc pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Ruby5kdev::Application.routes.draw do
   match 'contact_us' => 'misc#contact_us', :as => :contact_us
+  match 'newcomers'  => 'misc#newcomers',  :as => :newcomers
 
   devise_for :users
 
--- spec/routing/misc_routing_spec.rb
+++ spec/routing/misc_routing_spec.rb
@@ -2,9 +2,12 @@ require "spec_helper"
 
 describe MiscController do
   describe "routing" do
-
     it "routes to #contact_us" do
       get("/contact_us").should route_to("misc#contact_us")
+    end
+
+    it "routes to #newcomers" do
+      get("/newcomers").should route_to("misc#newcomers")
     end
   end
 end

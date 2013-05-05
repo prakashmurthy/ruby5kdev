38d4b7500f95b2a0768eeebe5c3fa58d03ef428e
Prakash Murthy <prakblr@yahoo.com>
2013-05-05 22:07:32 +0530

    Added tool tip on github & twitter icons; for now it is not useful

--- app/helpers/oauth_helper.rb
+++ app/helpers/oauth_helper.rb
@@ -3,7 +3,9 @@ module OauthHelper
     names.each do |name|
       concat(link_to(image_tag("icons/#{name}_32.png"),
         user_omniauth_authorize_path(name),
-        :id => "sign_in_with_#{name}"))
+        :id => "sign_in_with_#{name}",
+        :title => "Log in with #{name.to_s.titleize}",
+        :rel => "tooltip" ))
     end
     nil
   end

50f8a2802fb9eb6daa3babc6e63202e6e64ba59b
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-05-08 22:59:25 +0530

    Added user's gravatar in the comment view

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -3,13 +3,13 @@ module EntriesHelper
     entry.user == current_user && !current_user.nil?
   end
 
-  def gravatar_for(user)
+  def gravatar_for(user, size="80x80")
     if user.avatar_url.present?
       gravatar_url = user.avatar_url
     else
       gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
       gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     end
-    image_tag(gravatar_url, :size => "80x80", alt: user.email, class: "gravatar")
+    image_tag(gravatar_url, :size => size, alt: user.email, class: "gravatar")
   end
 end
